264bc17c86218e78e4bbb628e861154ea20d9d0b
Mehmet175 <76875926+Mehmet175@users.noreply.github.com>
2022-07-11 18:34:43 +0300

    .
    .

---
+++ AuthorizedDemo/AuthorizedDemo/IJWTAuthenticationManager.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorizedDemo
+{
+    public interface IJWTAuthenticationManager
+    {
+        string Authenticate(string username, string password);
+    }
+
+    public class JWTAuthenticationManager : IJWTAuthenticationManager
+    {
+        string _key;
+
+        public JWTAuthenticationManager(string key)
+        {
+            _key = key;
+        }
+
+        private readonly Dictionary<string, string> _users = new Dictionary<string, string>
+        {
+            {"usr1", "psr1" },
+            {"usr2", "psr2" },
+        };
+
+        public string Authenticate(string username, string password)
+        {
+            if(_users.Any(m => m.Key == username && m.Value == password))
+            {
+                return null;
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.ASCII.GetBytes(_key);
+            var tokenDescription = new SecurityTokenDescriptor()
+            {
+                // Gövdemin
+                Subject = new ClaimsIdentity(new Claim[] 
+                { 
+                    new Claim(ClaimTypes.Name, username)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescription);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ AuthorizedDemo/AuthorizedDemo/Startup.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorizedDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        private readonly string _key = "Bu benim uzun string key deðerim...";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AuthorizedDemo", Version = "v1" });
+            });
+
+            services.AddAuthentication(x => 
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x => 
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_key)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+            services.AddSingleton<IJWTAuthenticationManager>(new JWTAuthenticationManager(_key));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuthorizedDemo v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            // Authentication için eklendi...
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
